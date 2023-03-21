d0b610b76379a8f40b2f5e48098029c58146b220
Rafaela Romin Garcia <124751861+RafaelaRomin@users.noreply.github.com>
2023-03-21 13:38:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,26 +15,3 @@ async function getMovies(url) {
         console.log(error);
     }
 }
-
-async function displayMovies(url) {
-    const movies = await getMovies(url);
-    main.innerHTML = '';
-    movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie;
-        console.log(movie);
-        const movieEL = document.createElement('div');
-        movieEL.classList.add('movie');
-        movieEL.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}" >
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
-                <h3>Descrição</h3>
-                ${overview}
-            </div>
-        `;
-        main.appendChild(movieEL);
-    });
-}
